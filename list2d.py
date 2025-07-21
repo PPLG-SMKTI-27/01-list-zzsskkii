651ea76c91ca5e39f341e6e7536652afdfdbf4f4
zzsskkii <24_zaskiarahma@student.smkti.net>
2025-07-21 14:43:19 +0800

    list.py

---
+++ list2d.py
@@ -0,0 +1,13 @@
+nilai = [[10,20,30],
+         [40,50,60],
+         [70,80,90],
+]
+for i in range (len(nilai)):
+    index0 = nilai [0][0] + nilai [0][1] + nilai [0][2]
+    index1 = nilai [1][0] + nilai [1][1] + nilai [1][2]
+    index2 = nilai [2][0] + nilai [2][1] + nilai [2][2]
+
+print(index0)
+print(index1)
+print(index2)
+
